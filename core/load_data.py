5eb56f55aa54dd28b6621d40da4300c3d9e50562
Adam Murray <a.murray0413@gmail.com>
2024-10-27 21:55:17 -0700

    Update load_data.py

--- core/load_data.py
+++ core/load_data.py
@@ -2,7 +2,7 @@
 
 from peptide_dataset import PeptideDataset
 
-input_file = "/content/drive/MyDrive/Aa_Lokey_Lab/cyclic_peptide_training_set.csv"
+input_file = "training_data/cycpeptmpdb_cleaned_training_set.csv" # Set this to the path of the dataset
 
 dataset = PeptideDataset(input_file)
 dataset.clean()
